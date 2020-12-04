c98aaea1a78e5a308d8515e23d092485b9ec280a
vianey17 <vianey17@ru.is>
2020-12-04 17:11:17 +0000

    image buttons work

--- App.js
+++ App.js
@@ -1,21 +1,44 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Root, Container, Button } from 'native-base';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import { selectFromCameraRoll, takePhoto } from './src/services/ImagePickerService';
+export default class App extends React.Component {
+    async gallery(){
+        const photo = await selectFromCameraRoll();
+        this.setState({image:photo});
+    }
+
+    async takePhoto(){
+        const photo = await takePhoto();
+        this.setState({image:photo});
+    }
+    render(){
+        return (
+            <Root>
+                <Container>
+                    <View style={styles.View}>
+                        <Button large block onPress={()=>this.gallery()}>
+                            <Text>
+                                Grab image
+                            </Text>
+                        </Button>
+                        <Button large block success onPress={()=>this.takePhoto()}>
+                            <Text>
+                                Take photo
+                            </Text>
+                        </Button>
+                    </View>
+                    
+                </Container>
+            </Root>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    View: {
+        padding: 100
+    }
+})
